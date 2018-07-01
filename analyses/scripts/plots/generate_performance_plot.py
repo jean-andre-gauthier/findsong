1fe243fe0d2a781a183ea4e1ee016764c43268a1
Jean André Gauthier <jean.andre.gauthier@gmail.com>
2018-07-01 14:45:28 +0200

    Added performance plots

--- analyses/scripts/plots/generate_performance_plot.py
+++ analyses/scripts/plots/generate_performance_plot.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def main():
@@ -36,7 +37,8 @@ def main():
         plt_indexer_durations = list(
             map(lambda line: line[2], input_file_contents))
         plt_matcher_durations_averages = list(
-            map(lambda line: round(float(line[3])), input_file_contents))
+            map(lambda line: np.average(np.array(line[3:]).astype(np.int)),
+                input_file_contents))
 
         make_plot(plt_index_sizes, "Index size (# songs)",
                   plt_index_fingerprints, "# Fingerprints",
@@ -48,22 +50,47 @@ def main():
                   "Matcher average duration (ms)", args.outputplotmatchespath)
 
 
+def align_yaxis(ax1, v1, ax2, v2):
+    _, y1 = ax1.transData.transform((0, v1))
+    _, y2 = ax2.transData.transform((0, v2))
+    adjust_yaxis(ax2, (y1 - y2) / 2, v2)
+    adjust_yaxis(ax1, (y2 - y1) / 2, v1)
+
+
+def adjust_yaxis(ax, ydif, v):
+    inv = ax.transData.inverted()
+    _, dy = inv.transform((0, 0)) - inv.transform((0, ydif))
+    miny, maxy = ax.get_ylim()
+    miny, maxy = miny - v, maxy - v
+    if -miny > maxy or (-miny == maxy and dy > 0):
+        nminy = miny
+        nmaxy = miny * (maxy + dy) / (miny + dy)
+    else:
+        nmaxy = maxy
+        nminy = maxy * (miny + dy) / (maxy + dy)
+    ax.set_ylim(nminy + v, nmaxy + v)
+
+
 def make_plot(xs, xs_label, ys1, ys_label1, ys2, ys_label2, file_name):
     figure, axis1 = plt.subplots()
     axis1.set_xlabel(xs_label)
     axis1.set_ylabel(ys_label1, color="red")
     axis1.tick_params(axis='y', labelcolor="red")
     handle1, = plt.plot(xs, ys1, "r--", label=ys_label1)
+    ticks = [tick for tick in plt.gca().get_yticks() if tick >= 0]
+    plt.gca().set_yticks(ticks)
 
     axis2 = axis1.twinx()
     axis2.set_ylabel(ys_label2, color="blue")
     axis2.tick_params(axis='y', labelcolor="blue")
     handle2, = plt.plot(xs, ys2, "b--", label=ys_label2)
 
+    align_yaxis(axis1, 2700, axis2, 5000)
+
     figure.tight_layout()
     plt.legend(handles=[handle1, handle2], loc=1)
 
-    plt.savefig(file_name)
+    plt.savefig(file_name, transparent=True)
 
 
 if __name__ == "__main__":
