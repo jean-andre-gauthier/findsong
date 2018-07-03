ab5c7036b7edbbe6775152994ba34d52b21a09f6
Jean Andre Guathier <jean.andre.gauthier@gmail.com>
2018-07-03 21:39:25 +0200

    Corrected wrong parameter default values in documentation

--- src/main/scala/ja/gauthier/findsong/FindSong.scala
+++ src/main/scala/ja/gauthier/findsong/FindSong.scala
@@ -32,22 +32,22 @@ import scala.util.Success
  *                          Glob for the song files to index
  * -m, --matcherGlob <glob>
  *                          If present, the clip files matching the glob are used instead of the microphone
- * --maxMatches <value>     Maximal number of matches returned by the search engine (default = 5)
- * --peakDeltaF <value>     Frequency range in which a spectrogram cell has to be a local maximum to be considered a peak (default = 1)
- * --peakDeltaT <value>     Time range in which a spectrogram cell has to be a local maximum to be considered a peak (default = 1)
+ * --maxMatches <value>     Maximal number of matches returned by the search engine (default = 3)
+ * --peakDeltaF <value>     Frequency range in which a spectrogram cell has to be a local maximum to be considered a peak (default = 4)
+ * --peakDeltaT <value>     Time range in which a spectrogram cell has to be a local maximum to be considered a peak (default = 4)
  * --peaksPerChunk <value>  Maximal number of peaks in any given fingerprinting chunk (default = 2)
  * --samplesPerCapture <value>
  *                          Size of a microphone recording in samples (default = 80000)
  * --samplesPerChunk <value>
- *                          Size of a fingerprinting chunk in samples (default = 16)
+ *                          Size of a fingerprinting chunk in samples (default = 1024)
  * --samplesPerChunkStep <value>
- *                          Size of the stride between two fingerprinting chunks in samples (default = 8)
+ *                          Size of the stride between two fingerprinting chunks in samples (default = 512)
  * --sampleRate <value>     Fingerprinting / recording sample rate (default = 8000)
  * --scoreCoefficient <value>
  *                          Coefficient that is used in the match scoring function (default = 30)
- * --windowDeltaF <value>   Frequency range in which neighbouring peaks can be paired up (default = 1)
- * --windowDeltaT <value>   Time range in which neighbouring peaks can be paired up (default = 4)
- * --windowDeltaTi <value>  Minimal time difference for neighbouring peaks to be paired up (default = 2)
+ * --windowDeltaF <value>   Frequency range in which neighbouring peaks can be paired up (default = 30)
+ * --windowDeltaT <value>   Time range in which neighbouring peaks can be paired up (default = 35)
+ * --windowDeltaTi <value>  Minimal time difference for neighbouring peaks to be paired up (default = 5)
  * --yellowLevel <value>    Threshold for a match score to be displayed in yellow (default = 10)
  *
  */
